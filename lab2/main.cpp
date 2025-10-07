513f41b49a0296e16618c9731136e5630c176f63
mirka <mirkagreytan@gmail.com>
2025-10-07 11:32:21 +0300

    Gauss done

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -4,6 +4,7 @@
 
 int a, b;
 double matrix[3][4] = {{2 + K, 0.25, 0.75, 3 + K}, {0.25, 1.5 + K, 0.45, 2.2 + K}, {0.75, 0.45, 3 + K, 4.2 + K}};
+double init_matrix[3][4];
 
 void print_matrix(const std::string& label = "") {
     std::cout << "\n";
@@ -46,8 +47,6 @@ void solve_gauss() {
         return;
     }
 
-    //TODO переписать все так, чтобы работало с любыми матрицами(масштабировалось)
-
     // Делим все элементы второй строки на элемент 2 2
     double temp = matrix[1][1];
     for (int i = 3; i >= 0; i--) {
@@ -55,17 +54,47 @@ void solve_gauss() {
     }
     print_matrix();
 
-    // Прибавляем к строкам 1 и 3 вторую строку, умноженную на первый элемент строки с минусом
-    for (int i = 1; i < 3; i++) {
-        double kf = -matrix[i][0];
-        for (int j = 0; j < 4; j++) {
-            matrix[i][j] = matrix[i][j] + matrix[0][j] * kf;
-        }
+    // Прибавляем к 3 строке вторую, умноженную на второй элемент строки с минусом
+    double kf = -matrix[2][1];
+    for (int j = 0; j < 4; j++) {
+        matrix[2][j] = matrix[2][j] + matrix[1][j] * kf;
     }
     print_matrix();
+
+    // Проверка, что элемент 3 3 не равен нулю
+    if (matrix[2][2] == 0) {
+        std::cout << "a33 is zero!";
+        return;
+    }
+
+    // Делим все элементы второй строки на элемент 2 2
+    temp = matrix[2][2];
+    for (int i = 3; i >= 0; i--) {
+        matrix[2][i] = matrix[2][i] / temp;
+    }
+    print_matrix();
+
+    // Вычисляем неизвестные
+    double x1, x2, x3;
+    x3 = matrix[2][3];
+    x2 = matrix[1][3] - matrix[1][2] * x3;
+    x1 = matrix[0][3] - matrix[0][2] * x3 - matrix[0][1] * x2;
+
+    std::cout << "\n";
+    printf("x1 = %.6lf \n", x1);
+    printf("x2 = %.6lf \n", x2);
+    printf("x3 = %.6lf \n", x3);
+
+    // Проверка
+    for (int i = 0; i < 3; i++) {
+        double res = init_matrix[i][0] * x1 + init_matrix[i][1] * x2 + init_matrix[i][2] * x3;
+        std::cout << "\n";
+        printf("Expected: %.6lf Result: %.6lf", init_matrix[i][3], res);
+    }
 }
 
 int main() {
+    std::copy(&matrix[0][0], &matrix[0][0]+3*4,&init_matrix[0][0]);
     print_matrix("Initial matrix");
 
     solve_gauss();
