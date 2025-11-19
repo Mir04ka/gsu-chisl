f4505dfe96983ecc6cc70fec08d0164775c30b1b
mirka <mirkagreytan@gmail.com>
2025-11-19 15:54:03 +0300

    Lab3 Newton and combine methods done

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #define EPSILON 1e-3
 
@@ -83,25 +84,172 @@ double f2(double x) {
 }
 
 void solve_newton(double from, double to) {
+    cout << "\nМетод Ньютона\n";
+
     double init_pribl;
-
-    double der_odds[3];
-    int k = 3;
-    for (int i = 0; i < 3; i++) {
-        der_odds[i] = odds[i] * k;
-        k--;
+    if (f(from) * f2(from) > 0) {
+        cout << "\nУсловие выполняется для начала отрезка";
+        init_pribl = from;
+    }
+    else if (f(to) * f2(to) > 0) {
+        cout << "\nУсловие выполняется для конца отрезка";
+        init_pribl = to;
+    }
+    else {
+        cout << "\nУсловие не выполняется ни для одного конца отрезка!";
+        return;
     }
 
-    double der2_odds[2];
-    k = 2;
-    for (int i = 0; i < 2; i++) {
-        der2_odds[i] = der_odds[i] * k;
-        k--;
+    cout << "\nНачальное приближение: " << init_pribl << "\n";
+
+    double current_x = init_pribl - f(init_pribl) / f1(init_pribl);
+    double prev_x = init_pribl;
+    for (int i = 1; i < 100; i++) {
+        double eps = current_x - prev_x;
+        cout << i << ". " << current_x << " | Разность: " << eps << "\n";
+        if (eps < EPSILON) {
+            cout << "x=x" << i << "\n";
+
+            cout << "Проверка: \nЕсли полученный х подставить в начальное уравнение, то ответ будет: " << f(current_x) << " " << (f(current_x) < EPSILON ? "ОК" : "НЕВЕРНО") << "\n";
+
+            return;
+        }
+
+        prev_x = current_x;
+        current_x = prev_x - f(prev_x) / f1(prev_x);
     }
 
+    std::cout << "Достигнуто максимальное количество итераций! Ответ не найден";
+}
 
+void solve_hord(double from, double to) {
+    cout << "\nМетод хорд\n";
 
-    // TODO Newton method
+    double c;
+    if (f(from) * f2(from) > 0) {
+        cout << "\nУсловие выполняется для начала отрезка";
+        c = from;
+    }
+    else if (f(to) * f2(to) > 0) {
+        cout << "\nУсловие выполняется для конца отрезка";
+        c = to;
+    }
+    else {
+        cout << "\nУсловие не выполняется ни для одного конца отрезка!";
+        return;
+    }
+
+    cout << "\nНеподвижная точка: " << c << "\n";
+
+    double init_pribl;
+    if (f(from) * f(c) < 0) {
+        cout << "\nУсловие выполняется для начала отрезка";
+        init_pribl = from;
+    }
+    else if (f(to) * f(c) < 0) {
+        cout << "\nУсловие выполняется для конца отрезка";
+        init_pribl = to;
+    }
+    else {
+        cout << "\nУсловие не выполняется ни для одного конца отрезка!";
+        return;
+    }
+
+    cout << "\nНачальное приближение: " << init_pribl << "\n";
+
+    double current_x = init_pribl - (f(init_pribl) * (init_pribl - c)) / (f(init_pribl) - f(c));
+    double prev_x = init_pribl;
+    for (int i = 1; i < 100; i++) {
+        double eps = abs(current_x - prev_x);
+        cout << i << ". " << current_x << " | Разность: " << eps << "\n";
+        if (eps < EPSILON) {
+            cout << "x=x" << i << "\n";
+
+            cout << "Проверка: \nЕсли полученный х подставить в начальное уравнение, то ответ будет: " << f(current_x) << " " << (f(current_x) < EPSILON ? "ОК" : "НЕВЕРНО") << "\n";
+
+            return;
+        }
+
+        prev_x = current_x;
+        current_x = prev_x - f(prev_x) / f1(prev_x);
+    }
+
+    std::cout << "Достигнуто максимальное количество итераций! Ответ не найден";
+}
+
+void solve_combine(double from, double to) {
+    cout << "\nКомбинированный метод\n";
+
+    double a = from;
+    double b = to;
+
+    double c;     // неподвижная точка (для хорд)
+    bool newton_left; // true: Newton на a, false: Newton на b
+
+    if (f(a) * f2(a) > 0) {
+        cout << "Выполняется f(a) * f''(a) > 0 → a считаем методом Ньютона, b — методом хорд.\n";
+        newton_left = true;
+        c = a;
+    }
+    else if (f(b) * f2(b) > 0) {
+        cout << "Выполняется f(b) * f''(b) > 0 → b считаем методом Ньютона, a — методом хорд.\n";
+        newton_left = false;
+        c = b;
+    }
+    else {
+        cout << "Условие f(x)*f''(x)>0 не выполняется ни в одной точке отрезка!\n";
+        return;
+    }
+
+    cout << "Неподвижная точка c = " << c << "\n";
+
+    // Начальные приближения
+    double x_new;
+    double x_hord;
+
+    if (newton_left) {
+        x_new = a - f(a) / f1(a);
+        x_hord = b - f(b) * (b - c) / (f(b) - f(c));
+    } else {
+        x_new = b - f(b) / f1(b);
+        x_hord = a - f(a) * (a - c) / (f(a) - f(c));
+    }
+
+    cout << "Начаьное приближение:\nНьютона: " << x_new << "\nХорд: " << x_hord << "\n";
+
+    cout << "\nНачало итераций:\n";
+
+    for (int i = 1; i <= 100; i++) {
+
+        cout << "\nИтерация " << i << ":\n";
+        cout << "  Ньютон: x_new = " << x_new << "\n";
+        cout << "  Хорды:  x_hord = " << x_hord << "\n";
+
+        if (fabs(x_new - x_hord) < EPSILON) {
+            double ans = (x_new + x_hord) / 2;
+            cout << "\nКорень найден: x = " << ans << "\n";
+            cout << "Проверка: f(x) = " << f(ans) << "\n";
+            return;
+        }
+
+        if (newton_left) {
+            a = x_new;
+            b = x_hord;
+        } else {
+            b = x_new;
+            a = x_hord;
+        }
+
+        if (newton_left) {
+            x_new = a - f(a) / f1(a);
+            x_hord = b - f(b) * (b - c) / (f(b) - f(c));
+        } else {
+            x_new = b - f(b) / f1(b);
+            x_hord = a - f(a) * (a - c) / (f(a) - f(c));
+        }
+    }
+
+    cout << "\nДостигнуто максимальное количество итераций! Корень не найден.\n";
 }
 
 int main() {
@@ -109,8 +257,10 @@ int main() {
 
     cout << "\nУточняем корень уравнения x1 на [-3; -2.5] уравнения f(x) = x^3 + 2 * x^2 + 4\n";
 
-    solve_iter(-3, -2.5);
-    solve_newton(-3, -2.5);
+    // solve_iter(-3, -2.5);
+    // solve_newton(-3, -2.5);
+    // solve_hord(-3, -2.5);
+    solve_combine(-3, -2.5);
 
     return 0;
 }
